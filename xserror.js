3c5ec76fa5a32deaf30d22a93ffc2d1ed0ddfcea
Alfred Bratterud <alfred.bratterud@hioa.no>
2013-01-23 20:58:43 +0100

    Separated javascript from HTML, intented

---
+++ xserror.js
@@ -0,0 +1,45 @@
+
+var txt;
+var txt1;
+var txt2="foo";
+var txt3;
+var prevBt="undefined";
+$(document).ready(function(){
+
+
+    function loadfail(){
+	alert("Fakkapp!");
+    }
+    
+    function parse(xml){
+	$("#output").append("ya");
+	$(this).find("text").each(function(){
+	    $("#output").empty();
+	    $("#output").append($(this).find('text').text());
+	});
+	//$("#output").append("yo");
+	//$("#output").append($(this).find('text').text());
+    }
+
+    $("button").click(function(){
+
+
+	$(this).fadeTo(1000,0.5);//fades out currently pressed button
+
+	$("#"+prevBt).fadeTo(1000,1);//fade in previous button
+
+	//$("#output").load("tekst.txt");
+
+	$.ajax({
+	    type: "GET", 
+	    URL:"www.xserror.com/stuff.xml", 
+	    dataType:"html", 
+	    success: parse,
+	    error: loadfail
+	    
+	});
+	prevBt=this.id;//sets previous button
+	//$("#output").html(txt);
+
+    });
+});
